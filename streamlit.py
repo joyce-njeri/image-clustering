ca693fc927d267bebf4821819428098fc4bbc4bd
joyce-njeri <jmiiri17@alustudent.com>
2022-02-03 17:45:43 +0300

    first commit

---
+++ streamlit.py
@@ -0,0 +1,27 @@
+from turtle import width
+import streamlit as st
+from PIL import Image
+import os
+
+st.write(
+    """
+    # Google Images Clustering
+    A Machine Learning App that clusters product images according to either the similarity of the images or the similarity of the texts that describe the images.
+    """
+)
+
+directory = 'C:\\Users\\Joyce\\Documents\\Clustering images\\Output\\'
+clusters = []
+images = []
+
+col1, col2, col3 = st.columns(3)
+
+i = 1
+for folder in os.listdir(directory):
+    clusters.append(folder)
+    st.header('Cluster ' + str(i))
+    for filename in os.listdir(directory+folder):
+        images.append(filename)
+        image = Image.open(directory+folder+'\\'+filename)
+        st.image(image, caption=filename, width=500, use_column_width=True)
+    i+=1
